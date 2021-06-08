d9b1e600c249e388932d6d2afdb0b14e7a3dbf7c
Malena Gonzalez <male_111_gonzalez@hotmail.com>
2021-06-08 20:13:11 -0300

    Agregar archivos de proyecto.

---
+++ GestOn/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestOn.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
