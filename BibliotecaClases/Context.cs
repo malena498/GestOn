93fbb5fbd2bfbfb45c530ea900f27da0af60f26e
Malena Gonzalez <male_111_gonzalez@hotmail.com>
2021-06-08 21:24:26 -0300

    dbb

--- BibliotecaClases/Context.cs
+++ BibliotecaClases/Context.cs
@@ -3,12 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.IO;
+
 
 namespace BibliotecaClases
 {
-    class Context: DbContext
+    class Context : DbContext
     {
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Pedido> Pedidos { get; set; }
+
+        public Context()
+             : base("GestOn")
+        {
+            ConfigureForSqlServer();
+        }
+        public static void ConfigureForSqlServer()
+        {
+            try
+            {//Local 
+                String baseConnectionString = @"Data Source=LAPTOP-9SVDO2G2\SQLEXPRESS;user id=sa;password=123456789;MultipleActiveResultSets=True";
+
+
+                Database.DefaultConnectionFactory = new System.Data.Entity.Infrastructure.SqlConnectionFactory(baseConnectionString);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
