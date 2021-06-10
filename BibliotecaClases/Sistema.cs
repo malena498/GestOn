fdcbb732872286eb0214f9f1ec89cf31a8b553e1
Malena Gonzalez <male_111_gonzalez@hotmail.com>
2021-06-10 19:59:03 -0300

    sistema 10/06/2021

---
+++ BibliotecaClases/Sistema.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BibliotecaClases
+{
+    public class Sistema
+    {
+        private static Sistema sistema;
+        public static Sistema GetInstancia()
+        {
+
+            if (sistema == null)
+            {
+                sistema = new Sistema();
+
+            }
+
+            return sistema;
+        }
+    }
+}
