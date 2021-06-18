b948ee3b66ec9971ef69bcef54d40e58c16a683a
Malena Gonzalez <male_111_gonzalez@hotmail.com>
2021-06-17 22:30:20 -0300

    page master

--- GestOn2/PaginasMaestras/PageMaster.Master.designer.cs
+++ GestOn2/PaginasMaestras/PageMaster.Master.designer.cs
@@ -22,13 +22,13 @@ namespace GestOn2 {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
         
         /// <summary>
-        /// Control head.
+        /// Control ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
         
         /// <summary>
         /// Control HyperLink18.
