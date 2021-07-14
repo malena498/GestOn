673173bb356bf5343730ec130c65cecb2301cc15
Malena Gonzalez <male_111_gonzalez@hotmail.com>
2021-07-14 17:30:47 -0300

    CLASE

--- BibliotecaClases/Clases/Documento.cs
+++ BibliotecaClases/Clases/Documento.cs
@@ -6,11 +6,27 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 
-namespace BibliotecaClases
+namespace BibliotecaClases.Clases
 {
     public class Documento
     {
         [Key]
         public int IdDocumento { get; set; }
+        public String Formato { get; set; }
+        public bool EsDobleFaz { get; set; }
+        public bool AColor { get; set; }
+        public String Descripcion { get; set; }
+        public bool esEnvio { get; set; }
+        public String Direccion { get; set; }
+        public String gradoLiceal { get; set; }
+        public String Tipo { get; set; }
+        public bool EsPractico { get; set; }
+        public String NroPractico { get; set; }
+        public String NombreDocumento { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public bool Activo { get; set; }
+        public int UserId { get; set; }
+        public String ruta { get; set; }
+
     }
 }
